e474f6068e453f89f7d199ab54ce805438e962e4
harishjartarghar <harish.jartarghar@gmail.com>
2020-03-04 15:05:59 +0000

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require("express"),
     app=express();
     mongoose=require("mongoose");
     
-
+app.use(express.static(__dirname + '/public'));
 mongoose.connect("mongodb+srv://harish:8762448499@cluster0-rgopw.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true ,useUnifiedTopology: true});
 
 
@@ -13,7 +13,7 @@ app.get("/",function(req,res)
 });
 
 app.use("/home",require("./routes/index"));
-
+app.use("/user",require("./routes/users"));
 app.listen(5000,function()
 {
     console.log("server started at http://localhost:3000")
